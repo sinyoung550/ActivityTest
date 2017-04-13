ce9ad5dcbfcc62b9870a7ecf50b6d0ea7ac286e8
sinyoung550 <sinyoung550@gamil.com>
2017-04-13 20:21:53 +0900

    #3 EventPromming

--- app/src/main/java/com/example/parksinyoung/activitytest/MainActivity.java
+++ app/src/main/java/com/example/parksinyoung/activitytest/MainActivity.java
@@ -1,12 +1,24 @@
 package com.example.parksinyoung.activitytest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button butMain = (Button)findViewById(R.id.but_main);
+        butMain.setOnClickListener(butMainHendler);
     }
+    View.OnClickListener butMainHendler=new View.OnClickListener() {//이벤트핸들러의 객체
+        @Override
+        public void onClick(View v) {
+              Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+            startActivity(intent);
+        }
+    };
 }
