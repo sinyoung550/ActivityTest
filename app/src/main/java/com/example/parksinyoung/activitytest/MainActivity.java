33f0bdad211ea293ffcd70ffe63fbd67cfedaf62
sinyoung550 <sinyoung550@gamil.com>
2017-04-13 20:51:21 +0900

    #5 Transfer the value you typed to SecondActivity

--- app/src/main/java/com/example/parksinyoung/activitytest/MainActivity.java
+++ app/src/main/java/com/example/parksinyoung/activitytest/MainActivity.java
@@ -5,19 +5,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editName,editTel;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button butMain = (Button)findViewById(R.id.but_main);
         butMain.setOnClickListener(butMainHendler);
+        editName=(EditText)findViewById(R.id.Edit_name);
+        editTel=(EditText)findViewById(R.id.Edit_tel);
     }
     View.OnClickListener butMainHendler=new View.OnClickListener() {//이벤트핸들러의 객체
         @Override
         public void onClick(View v) {
               Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+            String name = editName.getText().toString(); //변수 name에 저장
+            String tel = editTel.getText().toString(); //변수 tel에 저장
+            intent.putExtra("name",name); //putExtra(이름,값);
+            intent.putExtra("name",tel);
             startActivity(intent);
         }
     };
--- app/src/main/java/com/example/parksinyoung/activitytest/SecondActivity.java
+++ app/src/main/java/com/example/parksinyoung/activitytest/SecondActivity.java
@@ -1,18 +1,28 @@
 package com.example.parksinyoung.activitytest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class SecondActivity extends AppCompatActivity {
-
+    String name,tel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        String name,tel;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         Button butBack = (Button)findViewById(R.id.but_second);
         butBack.setOnClickListener(butBackHandler);
+        Intent intent = getIntent();
+        name=intent.getStringExtra("name");
+        tel = intent.getStringExtra("tel");
+        TextView textResult=(TextView)findViewById(R.id.text_result);
+        textResult.setText("* 성명: "+name+"\n * 전화번호: "+tel);
     }
     View.OnClickListener butBackHandler = new View.OnClickListener() {
         @Override
